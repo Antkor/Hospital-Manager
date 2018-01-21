53b2b22bb0020911f54fb519bf556c76cf489f2a
Antkor <antkorsc@gmail.com>
2018-01-21 15:24:02 +0200

    added method to get all patients in dbconnector

--- src/DbConnector.java
+++ src/DbConnector.java
@@ -45,4 +45,31 @@ public class DbConnector {
 			
 		}
 	}
+	
+	public ArrayList<Patient> getAllPatients() {
+		ArrayList<Patient> patients = new ArrayList<Patient>();
+		openConnection();
+		try {		
+			
+			ResultSet rs = st.executeQuery("select * from patient");
+			
+			while(rs.next()) {				
+				
+				System.out.println(rs.getString("firstname"));				
+				Patient p = new Patient(rs.getString("amka"), rs.getString("lastname"), rs.getString("firstname"), rs.getString("address"), 
+										rs.getString("telephone"), rs.getString("email"), rs.getInt("age"), rs.getString("gender"), 
+										rs.getString("bloodtype"), rs.getString("insurance"), rs.getString("info"));
+				
+				patients.add(p);
+
+			}
+		}
+		catch (Exception e) {
+			return null;
+		}
+		finally {
+			closeConnection();
+		}
+		return patients;
+	}
 }
