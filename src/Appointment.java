2eb4b3e7f426bab7773bfccb7c38909ee7601706
pfakriadis <pfakriadis@gmail.com>
2018-01-22 19:23:02 +0200

    Implementation of attributes in Appointment class

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,4 +1,50 @@
+import java.util.Date;
 
 public class Appointment {
 
+	private Date day;
+	private String time;
+	private String amkaPatient;
+	private int arMitrwoyDoctor;
+	private String info;
+	private DbConnector connector;
+
+	public Appointment(Date day, String time, String amkaPatient, int arMitrwoyDoctor, String info) {
+		this.day = day;
+		this.time = time;
+		this.amkaPatient = amkaPatient;
+		this.arMitrwoyDoctor = arMitrwoyDoctor;
+		this.info = info;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public Date getDay() {
+		return day;
+	}
+	public void setDay(Date day) {
+		this.day = day;
+	}
+	public String getAmkaPatient() {
+		return amkaPatient;
+	}
+	public void setAmkaPatient(String amkaPatient) {
+		this.amkaPatient = amkaPatient;
+	}
+	public int getArMitrwoyDoctor() {
+		return arMitrwoyDoctor;
+	}
+	public void setArMitrwoyDoctor(int arMitrwoyDoctor) {
+		this.arMitrwoyDoctor = arMitrwoyDoctor;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
 }
